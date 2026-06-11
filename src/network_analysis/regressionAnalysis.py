917f88701624c70f1fe14a8b22721c7b2ca613a6
Ance Bulina <a.bulina@student.tue.nl>
2026-06-11 15:59:32 +0200

    regression progress

--- src/network_analysis/regressionAnalysis.py
+++ src/network_analysis/regressionAnalysis.py
@@ -2,6 +2,8 @@ import pandas as pd
 from pathlib import Path
 from src.network_analysis.scores import aggregate_lsoas_to_wards
 from duckdb import connect
+import statsmodels as smf
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # load scores dataframe generated from "scores" file
 scores_df = pd.read_parquet(BASE_DIR / "data" / "ward_brokerage_scores.parquet")
@@ -19,46 +21,40 @@ df = conn.execute(sql).df()
 df['period'] = df['year'].astype(str) + '-' + df['month_num'].astype(str).str.zfill(2)
 df = df.drop(['year', 'month_num'], axis=1)
 
-print(df.head())
+# print(df.head())
 new_df = aggregate_lsoas_to_wards(df)
 
 
-# Print all column names
-print(new_df.columns.tolist())
-print(new_df.head())
+# print(new_df.columns.tolist())
+# print(new_df.head())
 
 # add total crime per month column and  total crime for specific crime type column
-# new_df = new_df({"Total crime"} : kn, {"Total violence" : kn})
-# new_df["Total crime"] =
-# new_df["Total violence"] =
+total_crime = new_df.groupby(['WD21CD', 'period'], as_index=False)['count'].sum()
+total_crime = total_crime.rename(columns={'count': 'total_crime_in_month'})
+df = pd.merge(new_df, total_crime, on=['WD21CD', 'period'], how='left')
 
-def get_crime_count(crime_type, area, timeColumn: str) -> list:
-    '''
-    Given an area and a crime type, the function returns a list of crime counts for each month given.
-    '''
-    filteredData = df[(df['LSOA name'] == area) & (df['Crime type'] == crime_type)]
-    all_months = sorted(df[timeColumn].unique())
-    crimeCount = filteredData[timeColumn].value_counts()
-    crimeCount = crimeCount.reindex(all_months, fill_value=0)
+# print(df.head())
 
-    return list(crimeCount)
+#Create the "Future Violence" column that stores count per month
 
-# df = df.sort_values(by=['LSOA name', 'Month'])
-# violenceCount = get_crime_count('Violence', area, 'Month')
+violence = df[df['crime_type'] == 'Violence and sexual offences']
+violence_per_month = violence.groupby(['WD21CD', 'period'], as_index=False)['count'].sum()
+violence_per_month = violence_per_month.rename(columns={'count': 'violence_count'})
 
-#Create the "Future Violence" column
-# We group by LSOA so we don't accidentally shift one neighborhood's data into another
-# df['violenceNextMonth'] = df.groupby('LSOA name')['violenceCount'].shift(-1)
+#### merge dataframes again
+final_df = pd.merge(df, violence_per_month, on=['WD21CD', 'period'], how='left')
+print(final_df.head())
 
-# df = df.dropna(subset=['violenceNextMonth'])
+# fill in zeros instead of NaN so regression works
+df['violence_per_month'] = df['violence_per_month'].fillna(0)
 
-#### merge both dataframes
 # finally run the regression
 
-# equation = "violenceNextMonth ~ brokerageScore + totalCrimeCount"
+equation = "violence_per_month ~ brokerageScore + total_crime_in_month"
 
-# model = smf.glm(formula=equation,
-#                 data=df,
-#                 family=smf.families.NegativeBinomial(alpha=1.0)).fit()
-#
-# print(model.summary())
+model = smf.glm(formula=equation,
+                data=df,
+                family=smf.families.NegativeBinomial(alpha=1.0)).fit()
+
+print(model.summary())
+
